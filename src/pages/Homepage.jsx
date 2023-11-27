713cc3638bfec841656424815d33231bf543dcf3
Sean <seanms418@gmail.com>
2023-11-27 14:59:47 +1100

    Moved images to the public folder. Added a react component figure with figure image and figure caption for the life expectancy since 1970.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,32 @@
+import Figure from 'react-bootstrap/Figure';
+import { FigureCaption } from 'react-bootstrap';
 
 const Homepage = () => {
   return (
-    <section>
-      Learn about how the world has never been better
-    </section>
+    <>
+      <section className='container'>
+        Learn about how the world has never been better
+      </section>
+      <section className='container'>
+        <Figure>
+          <Figure.Image 
+            src='../public/assets/images/lifeExpectancyGloballySince1970.png'
+            height='500'
+            width='500'
+            alt='Life Expectancy Globally Since 1970'
+            fluid
+          />
+          <Figure.Caption>
+            Data source: UN WPP (2022); HMD (2023); Zijdeman et al. (2015); Riley (2005)
+          </Figure.Caption>
+          <FigureCaption>
+            <a href='https://ourworldindata.org/life-expectancy'>
+              OurWorldInData.org/life-expectancy
+            </a>
+          </FigureCaption>
+        </Figure>
+      </section>
+    </>
   );
 };
 
